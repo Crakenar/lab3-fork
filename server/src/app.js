7f0f8fca90afd96781707e6969b856916c28ebab
andrii <alo.andretti@gmail.com>
2024-01-07 15:28:06 +0200

    updated default ports

--- server/src/app.js
+++ server/src/app.js
@@ -34,7 +34,7 @@ User.createTable().then(() => {
 });
 
 // Start the server
-const port = process.env.PORT || 3001;
+const port = process.env.SERVER_PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 module.exports = app;
