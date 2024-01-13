52e5ebe3cea43d3b00fc1144ee1305e3a10c6097
Crakenar <teothou@hotmail.fr>
2024-01-13 01:54:44 +0100

    Rollback http port config

--- frontend/src/http-common.ts
+++ frontend/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URL_DOCKER ?? 'http://localhost:3001',
+  baseURL: import.meta.env.VITE_API_URL_DOCKER ?? 'http://localhost:3000',
   withCredentials: true,
 });
 
